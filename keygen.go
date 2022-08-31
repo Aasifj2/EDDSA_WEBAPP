12369ab309f0660ea10aef19ab39b9e5e9148521
roshgregory <roshangnoronha@gmail.com>
2022-08-31 16:54:15 +0530

    w

--- keygen.go
+++ keygen.go
@@ -1137,7 +1137,7 @@ func send_data(peer_details_list []string, value string, name string, protocolID
 		// log.Println(peer_details_list)
 		// log.Println(i, p2p.Host_ip, item, my_index)
 		//log.Println(i, my_index, p2p.Host_ip, item)
-		if i == my_index {
+		if i == my_index+1 {
 			continue
 		}
 		addr, _ := multiaddr.NewMultiaddr(item)
@@ -1202,13 +1202,13 @@ func wait_until(phase int) {
 		}
 		if flag == 1 {
 
-			time.Sleep(time.Millisecond * 10)
+			time.Sleep(time.Millisecond * 20)
 			// log.Println(flag, phase, receive_peer_phase, sent_peer_phase)
 			flag = 0
 			continue
 		}
 		fmt.Println("Returning from phase ", phase)
-		// time.Sleep(time.Second)
+		time.Sleep(time.Second)
 		// log.Println(phase, receive_peer_phase, sent_peer_phase)
 		return
 
@@ -1242,11 +1242,20 @@ func Wait_until_for_sign(phase int, t int) {
 			continue
 		}
 		fmt.Println("Returning from phase ", phase)
-		// time.Sleep(time.Second)
+		time.Sleep(time.Second)
+		for i, item := range peer_details_list {
+			item = strings.Split(item, "/")[len(strings.Split(item, "/"))-1]
+			if i == my_index {
+				continue
+			}
+			phase -= receive_peer_phase[item]
+			phase -= sent_peer_phase[item]
+
+		}
 		// log.Println(phase, receive_peer_phase, sent_peer_phase)
 		return
-
 	}
+
 }
 func GeneratePrime(size int) *big.Int {
 	prime, err := rand.Prime(rand.Reader, size)
